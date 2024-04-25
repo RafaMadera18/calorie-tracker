b1e023b1fe1922bd0c9d4a5e7d7032d71c9416a2
RafaMadera18 <rafa_madlop@hotmail.com>
2024-04-25 12:22:59 -0600

    Estructura Componente Formulario

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -4,19 +4,43 @@ export default function Form() {
   return (
     <form className="space-y-5 bg-white shadow p-10 rounded-lg">
         <div className="grid grid-cols-1 gap-3">
-            <label htmlFor="category">Categoria:</label>
+            <label htmlFor="category" className="font-bold">Categoria:</label>
             <select 
                 id="category" 
                 className="border border-slate-500 p-2 rounded-lg bg-white w-full"
             >
                 {categories.map(category => (
-                    <option>
+                    <option key={category.id} value={category.id}>
                         {category.name}
                     </option>
                 ))}
             </select>
         </div>
 
+        <div className="grid grid-cols-1 gap-3">
+            <label htmlFor="activity" className="font-bold">Actividad:</label>
+            <input 
+                id="activity" 
+                type="text" 
+                className="border border-slate-300 p-2 rounded-lg"
+                placeholder="Ej. Comida, Jugo de Naranja, Ensalada, Ejercicio, Pesas Bicicleta"
+            />
+        </div>
+        
+        <div className="grid grid-cols-1 gap-3">
+            <label htmlFor="calories" className="font-bold">Calorias:</label>
+            <input 
+                id="calories" 
+                type="number" 
+                className="border border-slate-300 p-2 rounded-lg"
+                placeholder="Calorias. Ej. 300 o 500"
+            />
+        </div>
+
+        <input type="submit"
+            className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer"
+            value="Guardar Comida o Ejercicio"
+        />
     </form>
   )
 }
